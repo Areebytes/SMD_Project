be1e79469122b5f03c1799b40d90f7fbdbbcc915
Areebytes <l230656@lhr.nu.edu.pk>
2026-05-03 23:07:40 +0500

    fixed splash screen

--- app/src/main/java/com/example/smd_project/SplashActivity.java
+++ app/src/main/java/com/example/smd_project/SplashActivity.java
@@ -17,6 +17,10 @@ public class SplashActivity extends AppCompatActivity{
         setContentView(R.layout.activity_splash);
 
         splashLogo =findViewById(R.id.sLogo);
+        splashLogo.setAlpha(0f); // Start invisible
+        splashLogo.animate().alpha(1f).setDuration(1500); // Fade in over 1.5 seconds
+
+
 
         new Handler().postDelayed(()->{
             startActivity(new Intent(SplashActivity.this, AuthActivity.class));
